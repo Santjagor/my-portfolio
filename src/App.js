9cab69ff959dbb2f72e9229ad6c9e674ed3065ad
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-01-18 17:09:29 -0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
-import Home from './components/Home/Home';
+import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Technologies from './components/Technologies/Technologies'
 
 function App() {
+  const [section, setSection] = useState(<About></About>)
+  const [title, setTitle] = useState("Full Stack Web Developer")
+  const changeSection = (event) => {
+    if (event.target.name === 'About me') {
+      setSection(<About></About>)
+      setTitle("Full Stack Web Developer")
+    }
+    if (event.target.name === 'Projects') {
+      setSection(<Projects></Projects>)
+      setTitle("Projects")
+    }
+    if (event.target.name === 'Technologies') {
+      setSection(<Technologies changeTitle={changeTitle}></Technologies>)
+      setTitle("Technologies")
+    }
+  }
+  const changeTitle = (event) => {
+    if (event.type === "mouseenter") {
+      setTitle(event.target.name)
+    }
+    if (event.type === "mouseleave") {
+      setTitle('Technologies')
+    }
+  }
   return (
     <div className="App">
-      <Home></Home>
+      <div className="container">
+        <div className="box">
+          <div className="name">Santiago Martin Solavaggione</div>
+          <div className="title">{title}</div>
+          {section}
+          <Navbar changeSection={changeSection}></Navbar>
+        </div>
+      </div>
     </div>
   );
 }
