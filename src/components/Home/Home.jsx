747d8013a12abf680ecea72ce2daf073ff95568c
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-01-18 13:43:13 -0300

    Technologies+

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,17 +6,32 @@ import Technologies from '../Technologies/Technologies'
 
 export default function Overlay() {
     const [section, setSection] = useState(<About></About>)
+    const [title, setTitle] = useState("Full Stack Web Developer")
+    const changeSection = (event) => {
+        if (event.target.name === 'About me') {
+            setSection(<About></About>)
+            setTitle("Full Stack Web Developer")
+        }
+        if (event.target.name === 'Projects') {
+            setSection(<Projects></Projects>)
+            setTitle("Projects")
+        }
+        if (event.target.name === 'Technologies') {
+            setSection(<Technologies></Technologies>)
+            setTitle("Technologies")
+        }
+    }
     return (
         <div>
             <div className={styles.container}>
                 <div className={styles.box}>
                     <div className={styles.name}>Santiago Martin Solavaggione</div>
-                    <div className={styles.fswd}>Full Stack Web Developer</div>
+                    <div className={styles.title}>{title}</div>
                     {section}
                     <div className={styles.nav}>
-                        <div className={styles.navButton} onClick={() => setSection(<About></About>)}></div>
-                        <div className={styles.navButton} onClick={() => setSection(<Projects></Projects>)}></div>
-                        <div className={styles.navButton} onClick={() => setSection(<Technologies></Technologies>)}></div>
+                        <button name="About me" className={styles.navButton} onClick={changeSection}></button>
+                        <button name="Projects" className={styles.navButton} onClick={changeSection}></button>
+                        <button name="Technologies" className={styles.navButton} onClick={changeSection}></button>
                     </div>
                 </div>
             </div>
--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -2,8 +2,13 @@ import styles from './Technologies.module.css'
 
 export default function Overlay() {
     return (
-        <div className={styles.section}> Technologies
-            
+        <div className={styles.section}>
+            <img className={styles.image} src="JavaScript.png" alt="JS" />
+                <img className={styles.image} src="Node.png" alt="Node" />
+                <img className={styles.image} src="React.png" alt="React" />
+                <img className={styles.image} src="Redux.png" alt="Redux" />
+                <img className={styles.image} src="ExpressJS.png" alt="ExpressJS" />
+                <img className={styles.image} src="Postgresql.png" alt="PostgreSQL" />
         </div>
     )
 }
