7af8c7a565d893a6a153a9bdce5ea71424e3a337
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-01-19 13:52:44 -0300

    Styles+Refactor

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Technologies from './components/Technologies/Technologies'
 import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
 
 function App() {
   const [title, setTitle] = useState("Full Stack Web Developer")
@@ -13,22 +14,28 @@ function App() {
   const changeSection = (event) => {
     if (event.target.name === 'About me') {
       navigate('/')
-      setTitle("/Full Stack Web Developer")
-    }
-    if (event.target.name === 'Projects') {
+      setTitle('/Full Stack Web Developer')
+    } else if (event.target.name === 'Projects') {
       navigate('/projects')
-      setTitle("/Proyectos")
-    }
-    if (event.target.name === 'Technologies') {
+      setTitle('/Proyectos')
+    } else if (event.target.name === 'Technologies') {
       navigate('/technologies')
-      setTitle("/Tecnologías")
+      setTitle('/Tecnologías')
     }
   }
   const changeTitle = (event) => {
     if (event.type === "mouseenter") {
       setTitle('/' + event.target.name)
+    } else if (event.type === "mouseleave") {
+      resetTitle()
     }
-    if (event.type === "mouseleave") {
+  }
+  const resetTitle = () => {
+    if (location.pathname === "/") {
+      setTitle('/Full Stack Web Developer')
+    } else if (location.pathname === "/projects") {
+      setTitle('/Proyectos')
+    } else if (location.pathname === "/technologies") {
       setTitle('/Tecnologías')
     }
   }
@@ -39,7 +46,7 @@ function App() {
         <div className="title">{title}</div>
         <Routes>
           <Route path='/' element={<About></About>} ></Route>
-          <Route path='/projects' element={<Projects></Projects>} ></Route>
+          <Route path='/projects' element={<Projects changeTitle={changeTitle}></Projects>}></Route>
           <Route path='/technologies' element={<Technologies changeTitle={changeTitle}></Technologies>} ></Route>
         </Routes>
         <Navbar changeSection={changeSection}></Navbar>
