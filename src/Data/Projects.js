89233ea9b0a501d4aed524753a99292bb2e3e2af
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-01-22 11:55:40 -0300

    Responsive

--- src/Data/Projects.js
+++ src/Data/Projects.js
@@ -3,7 +3,7 @@ export const projects = [
         name: "SuperMarket",
         image: "SuperMarket.png",
         link: "https://supermarketshop.vercel.app/",
-        description: "Tienda online orientada a comercios pequeños y medianos (Proyecto grupal final - Boot Camp Henry). Nuestra idea fue desarrollar un producto sencillo y accesible para que el comerciante local pueda modernizar su negocio y diversificar sus canales de venta. Entre sus funcionalidades se encuentran: Admin Dashboard, Buscador, Filtros y Ordenamientos de productos, Registro con autenticación de terceros y Login, Panel de Administrador, Carrito y Pasarela de pagos",
+        description: "Tienda online orientada a comercios pequeños y medianos (Proyecto grupal final - Boot Camp Henry). Nuestra idea fue desarrollar un producto sencillo y accesible para que el comerciante local pueda modernizar su negocio y diversificar sus canales de venta. Entre sus funcionalidades se encuentran: Admin Dashboard, Buscador, Filtros y Ordenamientos de productos, Registro con autenticación de terceros y Login, Panel de Administrador, Carrito y Pasarela de pagos.",
         technologies:
             [
                 { name: "JavaScript", image: "JavaScript.png" },
